75ab0f91954d5611608080ff01f49aabd3e29522
Liz Boika <mdaicdxl@gmail.com>
2022-07-24 22:14:01 -0400

    commit tree c21317f921435e96051f79225e8868f5e99295b4

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(search) {
+
+    drivers.filter(function(name) {
+    return name = search;
+
+})
+}
+
+console.log(findMatching('Sammy'))
